32f8503b6757f121991145a9f5fa603361e1e844
win dos <clement0jo85.com>
2018-05-22 15:55:24 +0200

    reperage de l'heure dans le tableau OK

--- screen accueil mega/src/ecran.ino
+++ screen accueil mega/src/ecran.ino
@@ -1,8 +1,9 @@
 void afficheSchedule() {
-    for (size_t i = 0; i <= set; i++) {
+    for (size_t i = 1; i <= set; i++) {
         if(timeColour[i][0] == hour){
             Theure = i;
             Serial.print(Theure);
         }
+
     }
 }
